fb61774fd9994daac5c147c7c22b03b2b0e406fe
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2023-04-28 13:12:16 +0200

    Created schema contet to about

---
+++ _studio/schemas/documents/about.js
@@ -0,0 +1,22 @@
+export default {
+	title: 'About', 
+	name: 'about', 
+	type: 'document', 
+	fields: [
+		{
+			title: 'Title', 
+			name: 'title', 
+			type: 'string', 
+		}, 
+		{
+			title: 'History', 
+			name: 'history', 
+			type: 'text', 
+		}, 
+		{
+			title: 'Opening Hours', 
+			name: 'openingHours', 
+			type: 'string',
+		}, 
+	]
+}
