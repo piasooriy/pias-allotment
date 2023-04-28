1b8ae575a7a5da79486fe90505a602345d5fe50f
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2023-04-28 12:53:23 +0200

    Imported supply in schemas

--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -1,7 +1,9 @@
 import newsletter from "./documents/newsletter";
 import seedAndPlant from "./documents/seed-and-plant";
+import supply from "./documents/supply";
 
 export default [
 	newsletter,
 	seedAndPlant,
+	supply,
 ]
