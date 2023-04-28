554b81051554b01fcaf513df06494155244e5ee4
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2023-04-28 13:13:49 +0200

    Imported Settings to schema

--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -2,10 +2,12 @@ import newsletter from "./documents/newsletter";
 import seedAndPlant from "./documents/seed-and-plant";
 import supply from "./documents/supply";
 import about from "./documents/about";
+import settings from "./documents/settings";
 
 export default [
 	newsletter,
 	seedAndPlant,
 	supply,
 	about,
+	settings,
 ]
