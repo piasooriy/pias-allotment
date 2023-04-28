fbd7e181e2a1b27c5f7b160511e0cf67132d9996
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2023-04-28 13:13:28 +0200

    Imported about

--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -1,9 +1,11 @@
 import newsletter from "./documents/newsletter";
 import seedAndPlant from "./documents/seed-and-plant";
 import supply from "./documents/supply";
+import about from "./documents/about";
 
 export default [
 	newsletter,
 	seedAndPlant,
 	supply,
+	about,
 ]
