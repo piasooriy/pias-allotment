8975c6f0ce20c6a6e9fc095d79b13cbcb1444ec2
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2023-04-30 13:37:38 +0200

    Added announcement in settings

--- _studio/schemas/documents/settings.js
+++ _studio/schemas/documents/settings.js
@@ -6,13 +6,31 @@ export default {
 	type: 'document', 
 	fields: [
 		{
+			title:'Announcement', 
+			name: 'announcement', 
+			type: 'object', 
+			fields: [
+				{
+					title: 'Text', 
+					name: 'text', 
+					type: 'string',
+					vaidation: rule => rule.max(250).warning('Max 250 characters'),
+				}, 
+				{
+					title: 'Visible', 
+					name: 'visible', 
+					type: 'boolean', 
+				}
+			]
+		}, 
+		{
 			title: 'About', 
 			name: 'about', 
 			type: 'string', 
 		}, 
 		{
-			title: 'Contact', 
-			name: 'contact', 
+			title: 'Contact e-mail', 
+			name: 'contactEmail', 
 			type: 'string', 
 		}, 
 		{
