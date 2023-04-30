dcc13ddd1091d412717e39155db771bb393f167d
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2023-04-30 13:57:50 +0200

    Removed newsletter in structure

--- _studio/schemas/structure/newsletter.js
+++
@@ -1,9 +0,0 @@
-export default Structure => {
-	const { divider, editor, list, documentTypeListItem} =  Structure; 
-
-	return list()
-			.title('Newsletter')
-			.items([
-				documentTypeListItem('newsletter'),
-			])
-}
