2651ead1a3662c95c1b93397b7605b9057df55a1
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2023-05-11 15:02:08 +0200

    Fetched newsletterlist to main index

---
+++ _app/js/modules/newsletter-list.js
@@ -0,0 +1,53 @@
+import {sanity} from "../sanity.js";
+
+export default async function newsletterList() {
+	const query = `*[_type == 'newsletter']{
+		title,
+		"image": image.asset->url,  
+		"altText": image.alt,
+		slug,
+	 }`;
+
+
+	const newsletters = await sanity.fetch(query);
+	
+	console.log(newsletters);
+	
+	function createNewsletterListContainerDOM() {
+	const newsletterListContainer = document.createElement('div');
+	newsletterListContainer.className = 'newsletter-list';
+	
+	
+		for (const newsletter of newsletters) {
+			const newsletterCard = document.createElement('div');
+			const newsletterBox = document.createElement ('figure'); 
+			const newsletterImage = document.createElement('img');
+			const newsletterTitle = document.createElement('figcaption');
+
+			newsletterListContainer.appendChild(newsletterCard);
+			newsletterListContainer.appendChild(newsletterTitle);
+			newsletterBox.appendChild(newsletterImage);
+
+			newsletterCard.className = 'newsletter__card';
+			newsletterBox.className = 'newsletter__box';
+
+			newsletterImage.className = 'newsletter__image';
+			newsletterImage.src = newsletter.image;
+			newsletterImage.alt = newsletter.altText;
+
+			newsletterTitle.className = 'newsletter__title';
+			newsletterTitle.innerText = newsletter.title;
+		}
+
+		return newsletterListContainer;
+		
+	}
+	
+	function renderHTML(){
+		const newsletterListContainer = createNewsletterListContainerDOM();
+		document.body.appendChild(newsletterListContainer);
+	}		
+
+	renderHTML();
+	
+};
