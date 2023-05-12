046de3074013d1d99b4740905716629df730758a
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2023-05-12 13:27:10 +0200

    Added comments on todo tasks

--- _app/js/modules/seed-supply-list.js
+++ _app/js/modules/seed-supply-list.js
@@ -10,13 +10,32 @@ export default async function seedAndSupply() {
 
 	const products = await sanity.fetch(query);
 	
+	/*****
+	 * @todo How to include the fetch of supplies in the query?
+	 * it would be easier if I could fetch all the supplies and seeds/plants as products. 
+	 * and then perhaps filter it according to either seedAndPlant type or supply type.
+	 * *[_type == 'supply']{   
+		title, 
+		"image": image.asset->url,  
+		"altText": image.alt,
+		slug,
+		} 
+	 */
+
+
 	console.log(products);
+/***
+ * @todo Move all fetch functions outside of the module, easier to 
+	reasue the fetch for other modules, in case you need it to make a 
+	seperate search filter, etc. You can import it the way you do
+	{sanity}
+ */
+	
 	
 	function createproductListContainerDOM() {
 	const productListContainer = document.createElement('div');
-	productListContainer.className = 'product-list';
-	
-	
+	productListContainer.className = 'product-list grid';
+
 		for (const product of products) {
 			const productCard = document.createElement('div');
 			const productBox = document.createElement ('figure'); 
@@ -24,11 +43,11 @@ export default async function seedAndSupply() {
 			const productTitle = document.createElement('figcaption');
 
 			productListContainer.appendChild(productCard);
-			productListContainer.appendChild(productTitle);
 			productCard.appendChild(productBox)
+			productCard.appendChild(productTitle);
 			productBox.appendChild(productImage);
 
-			productCard.className = 'product__card';
+			productCard.className = 'product__card grid__column--3';
 			productBox.className = 'product__box';
 
 			/* By using className you remove the other classnames and use
