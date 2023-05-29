06819df21dfd1a8cebed48c0c11b9a72f87a5d74
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2023-05-29 18:56:12 +0200

    Included newsletter-post in utils, for slug

--- _app/js/util/utils.js
+++ _app/js/util/utils.js
@@ -2,7 +2,7 @@ export function readSlug() {
 	const allUrl = window.location.href;
 	if(allUrl.includes('product')) {
 		return window.location.search.slice(1)
-	} if (allUrl.includes('newsletter')) {
+	} if (allUrl.includes('newsletter-post')) {
       return window.location.search.slice(1)
    } return undefined;
  }
