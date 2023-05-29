b63c2e554896a802ff14560a9abe234cf2130d6f
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2023-05-29 18:54:22 +0200

    Tried splitting fetch product query

--- _app/js/modules/fetch-product.js
+++ _app/js/modules/fetch-product.js
@@ -1,48 +1,50 @@
 import { sanity } from "../sanity.js";
 
 export default async function FetchProduct() {
-	const seedAndPlantQuery = `*[_type == 'seedAndPlant']{   
-		_id,  
-		title,  
-		_type,
-		productFormat,
-		durability,  
-		"image": picture.asset->url,  
-		"altText": picture.alt,
-		"slug":slug.current,
-		description, 
-		"stock": metadata.stock, 
-		"price": metadata.price, 
-		"family": family._ref,  
-		category,  
-		 }
+	/**
+	 * 
+	 * @NOTE Everything that is commented out
+	 * is just me trying to see if I can fetch both seed and plant as well as 
+	 * supplies under one function, which I highly doubt, but hey, 
+	 * no harm in trying.
+	 */
+	
+	const productQuery = `{
+		"seedAndPlant": *[_type == 'seedAndPlant']{       
+		 _id,  
+		 title,  
+		 _type,
+		 productFormat,
+		 durability,  
+		 "image": picture.asset->url,  
+		 "altText": picture.alt,
+		 "slug":slug.current,
+		 description, 
+		 "stock": metadata.stock, 
+		 "price": metadata.price, 
+		 "family": family._ref,  
+		 category,
+	  },
+	  "supply": *[_type == 'supply']{
+			_id,
+			  title, 
+			  _type, 
+			  "image": image.asset ->url,
+			  "altText": image.alt,
+			  "slug": slug.current,
+			  description,
+			  metadata
+		}
+	 }
 		`;	 
 
-	const SupplyQuery = `*[_type == 'supply']{   
-		_id,  
-		title,  
-		_type,
-		"image": image.asset->url,  
-		"altText": image.alt,
-		"slug":slug.current,
-		description, 
-		metadata, 
-		 }`;	 
-
 	 /*****
-	 * @todo How to include the fetch of supplies in the query?
-	 * it would be easier if I could fetch all the supplies and seeds/plants as products. 
-	 * and then perhaps filter it according to either seedAndPlant type or supply type.
-	 * *[_type == 'supply']{   
-		title, 
-		"image": image.asset->url,  
-		"altText": image.alt,
-		slug,
-		} 
+	 * @NOTE Fetching both supply and seedAndPlant in one query. 
+	 * 
 	 */
 
 	
-	const products = await sanity.fetch(seedAndPlantQuery, SupplyQuery);
+	const products = await sanity.fetch(productQuery);
 
 	return products;
 
