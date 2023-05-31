fc0fbfeaff76e0c23123f03f6f1baece8f62157e
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2023-05-31 20:46:08 +0200

    Imported toggle to main

--- _app/js/main-newsletter-post.js
+++ _app/js/main-newsletter-post.js
@@ -1,8 +1,10 @@
 //import FetchSingleNewsletterPost from "./modules/fetch-single-newsletter.js";
 import GetNewsletterPost from "./modules/newsletter-post.js";
+import ToggleNewsletter from "./modules/toggle-newsletter.js";
 import EmailInput from "./modules/email-input.js";
 
 
 //FetchSingleNewsletterPost();
 GetNewsletterPost();
+ToggleNewsletter();
 EmailInput();
