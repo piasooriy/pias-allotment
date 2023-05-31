035ec29c6d1cf40f3e8f7c9e5537d5286162a629
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2023-05-31 20:46:38 +0200

    Created toggle in products page

---
+++ _app/js/modules/toggle-product.js
@@ -0,0 +1,34 @@
+
+export default function ToggleProduct() {
+	let informationVisible = false;
+
+	const toggleProductButton = document.querySelector('.product__container-information-toggle-button');
+	const toggleProductSection = document.querySelector('.product__container-information-toggle-section');
+
+	//EventListener
+	if (toggleProductButton) {
+		toggleProductButton.addEventListener('click', handleToggleProductButtonOnClick);
+	}
+
+	//Eventhandler
+
+	function handleToggleProductButtonOnClick(event) {
+		toggleInformation();
+		renderInformationHTML();
+	}
+
+	//Method
+
+	function toggleInformation() {
+		informationVisible = !informationVisible;
+	}
+
+	//render
+	function renderInformationHTML() {
+		if (informationVisible === true) {
+			toggleProductSection.classList.add('product__container-information-toggle-section--visible')
+		} else {
+			toggleProductSection.classList.remove('product__container-information-toggle-section--visible')
+		}
+	}
+}
