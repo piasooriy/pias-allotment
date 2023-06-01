c9db4a243bc7b0cdf3a9e16cf6a7d7a717e38af1
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2023-06-01 16:56:24 +0200

    Changed placement of comment

--- _app/js/util/utils.js
+++ _app/js/util/utils.js
@@ -1,3 +1,13 @@
+/**
+ * 
+ * @NOTE Redirecting to a new generated window, which uses the slug
+ * for product. 
+ * @SEE newsletterList function under newsletter-list.js and 
+ * getNewsletter function under newsletter-post.js
+ * https://developer.mozilla.org/en-US/docs/Web/API/Window/location
+ * https://developer.mozilla.org/en-US/docs/Web/API/Location/href
+ */
+
 export function readSlug() {
 	const allUrl = window.location.href;
 	if(allUrl.includes('product')) {
@@ -6,12 +16,3 @@ export function readSlug() {
       return window.location.search.slice(1)
    } return undefined;
  }
-
- /**
-  * 
-  * @NOTE Redirecting to a new generated window, which uses the slug
-  * for product. 
-  * @SEE seedAndSupply function
-  * https://developer.mozilla.org/en-US/docs/Web/API/Window/location
-  * https://developer.mozilla.org/en-US/docs/Web/API/Location/href
-  */
